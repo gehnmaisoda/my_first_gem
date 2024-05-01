d11e18ee802bbfbd57922cafea5b977e20c83a5d
shingo maeda <gehnmaisoda@gmail.com>
2024-05-01 21:32:35 +0900

    initial commit

---
+++ lib/my_first_gem.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "my_first_gem/version"
+
+module MyFirstGem
+  class Error < StandardError; end
+  # Your code goes here...
+  def self.hello
+    "Hello World"
+  end
+end
